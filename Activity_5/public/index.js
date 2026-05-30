e071d56755ed1bd416b2a9e41f2ff455380bfd01
DDDD908 <a01752908@tec.mx>
2026-05-29 23:42:18 -0600

    Complete menu pictures

--- Activity_5/public/index.js
+++ Activity_5/public/index.js
@@ -11,9 +11,18 @@ const menuImages = {
     "Cake": "https://sweetreservebakery.com/cdn/shop/files/BirthdayCakeGallery1.webp?v=1752999312",
     "Tea": "https://teashop.com/cdn/shop/articles/historia-del-chai-tea_520x500_b8d96e88-0ba4-42f8-a8f5-7f32118199eb.jpg?v=1734432684&width=2048",
     "Cookie": "https://yeyfood.com/wp-content/uploads/2025/02/WEB1clsoe_up_of_chocolate_chip_cookies._stacked_on_whi_07bc1482-455e-4c1a-969c-b0c7eaad50e1_3-720x720.jpg",
+    "Hot Cocoa": "https://sugarspunrun.com/wp-content/uploads/2024/01/Hot-cocoa-recipe-1-of-1-2.jpg",
+    "Brownie": "https://www.giallozafferano.es/images/288-28849/brownies-en-freidora-de-aire_1200x800.jpg",
+    "Mocha": "https://livinghealthywithchocolate.com/wp-content/uploads/2015/04/Healthy-Paleo-Starbucks-Mocha-Frappuccino-Recipe-dairyfree-glutenfree-sugarfree1.jpg",
+    "Sandwich": "https://editorialtelevisa.brightspotcdn.com/a1/91/54ede86c4a7c89ee98f239261143/sandwich-saludable-para-lunch.jpeg",
+    "Espresso": "https://www.novachef.es/media/images/espresso-macchiato.jpg",
+    "Donut": "https://www.marketresearchintellect.com/images/blogs/best-doughnut-brands.webp",
+    "Cold Brew": "https://thecookinglab.es/wp-content/uploads/2025/12/como-hacer-cold-brew-500x500.jpg",
+    "Waffle": "https://cravinghomecooked.com/wp-content/uploads/2019/02/easy-waffle-recipe-1-16.jpg",
+    "Milk Tea": "https://www.betterwithdairy.com/sites/default/files/2025-02/2_Boba-Milk-Tea.jpg",
+    "Croissant": "https://www.cocinadelirante.com/800x600/filters:format(webp):quality(75)/sites/default/files/images/2025/04/croissant-relleno-jamon-serrano.jpg"
 };
 
-// ===== MENU RENDERING =====
 async function changeMenu(day) {
 
     const map = {
