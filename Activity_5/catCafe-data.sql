eda2fd9862e8b61866993e7e49cc2e37cd649912
DDDD908 <a01752908@tec.mx>
2026-05-29 23:36:31 -0600

    Video

--- Activity_5/catCafe-data.sql
+++ Activity_5/catCafe-data.sql
@@ -14,8 +14,19 @@ COMMIT;
 
 SET AUTOCOMMIT=0;
 INSERT INTO MenuItems (item, price, dayOfWeek) VALUES
-('Latte',60.00,'Mon'),
+('Latte',67.00,'Mon'),
 ('Cake',50.00,'Mon'),
 ('Tea',40.00,'Tue'),
-('Cookie',30.00,'Tue');
+('Cookie',30.00,'Tue'),
+('Hot Cocoa',35.0,'Wed'),
+('Brownie',45.00,'Wed'),
+('Mocha',65.00,'Thu'),
+('Sandwich',70.0,'Thu'),
+('Espresso',50.00,'Fri'),
+('Donut',40.00,'Fri'),
+('Cold Brew',75.00,'Sat'),
+('Waffle',80.00,'Sat'),
+('Milk Tea',55.00,'Sun'),
+('Croissant',60.0,'Sun');
+
 COMMIT;
